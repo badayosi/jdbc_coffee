1105792357466f8d3c2d7e75014458182d2e6001
HyunChul Lee <hc89@naver.com>
2017-10-18 15:52:25 +0900

    Test DB 생성 및 자료입력

---
+++ src/kr/or/dgit/jdbc_setting/TestMain.java
@@ -0,0 +1,29 @@
+package kr.or.dgit.jdbc_setting;
+
+import java.sql.Connection;
+
+import kr.or.dgit.jdbc_setting.jdbc.DBCon;
+import kr.or.dgit.jdbc_setting.jdbc.jdbcUtil;
+import kr.or.dgit.jdbc_setting.service.DbService;
+import kr.or.dgit.jdbc_setting.service.ExportService;
+import kr.or.dgit.jdbc_setting.service.ImportService;
+import kr.or.dgit.jdbc_setting.service.InitService;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		DBCon dbCon = DBCon.getInstance();
+		Connection connection = dbCon.getConnection();
+		System.out.println(connection);
+		
+		DbService service = InitService.getInstance();
+		service.service();
+		service = ImportService.getInstance();
+		service.service();
+		service = ExportService.getInstance();
+		service.service();
+		
+		jdbcUtil.close(connection);
+		System.out.println("close!");
+	}
+}
